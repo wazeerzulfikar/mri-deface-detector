52dfcfd76a2ee53a60e386debdeca09617653a68
wazeer zulfikar <wazeer.zulfikar@gmail.com>
2018-07-25 10:33:01 +0530

    Cleaned code and Added comments

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const data_64 = require('./public/data/mri_64.json')
 var path = require('path')
 
 // var jimp = require('jimp')
-// var jimp = require('jimp');
-// var pica = require('pica');
+// var sharp = require('sharp');
+var pica = require('pica');
 
 
 var nifti = require('nifti-js')
@@ -31,9 +31,9 @@ async function loadModel(file, callback) {
 	callback(model);
 }
 
-function readFile(evt) {
+function readFile(e) {
 
-	file = evt.target.files[0];
+	file = e.target.files[0];
 
 	var reader = new FileReader();
 
@@ -45,72 +45,72 @@ function readFile(evt) {
 			if (!e.target.result) {
 				return;
 			} else {
-				var unzipped = pako.inflate(e.target.result);
-				// var unzipped = niftijs.decompress(e.target.result);
-				// console.log(unzipped)
-				// var niftiHeader = niftijs.readHeader(unzipped);
-				// var contents = niftijs.readImage(niftiHeader,unzipped);
 
+				// METHOD 1 - Using NIFTI-Reader-JS
+
+				// var file = e.target.result;
+				// if (niftijs.isCompressed(file)) {
+				// 	file = niftijs.decompress(file);
+				// }
+
+				// if (niftijs.isNIFTI(file)) {
+				// 	var niftiHeader = niftijs.readHeader(file);
+				// 	var image = niftijs.readImage(niftiHeader,file);
+				// }
+
+				// // Check if contents are correct using sum of pixels
+
+				// var check = new Uint8Array(image)
+				// console.log(check.reduce((a,b)=>a+b,0));
+
+
+				//  METHOD 2 - Using NIFTI-JS
+
+				var unzipped = pako.inflate(e.target.result);
 				var contents = nifti.parse(unzipped);
-				// normalArray = Array.prototype.slice.call(contents.data);
 				console.log(contents)
+
+				// Check if contents are correct using sum of pixels
+
 				var check = new Uint8Array(contents.data)
 				// const uniqueValues = [...new Set(check)]; 
 				console.log(check.reduce((a,b)=>a+b,0));
 
 
-				// console.log(contents.data.reduce((a,b)=>a+b,0))
-
-				// console.log(mat);
-				
 				// preprocess(contents);
-
 			}
 		}
 	};
 
-	reader.readAsBinaryString(file);
+	reader.readAsArrayBuffer(file);
 
 }
 
 function preprocess(contents) {
+	// Main function for preprocessing the contents of the NIFTI file, before feeding to model
 
 	normalArray = Array.prototype.slice.call(contents.data);
 	var mat = math.matrix(normalArray)
 	var dimensions = contents.sizes.slice()
 	console.log(dimensions)
-	// console.log(mat)
 
+	// Reshape takes too much time - Need to check
 	// mat = math.reshape(mat,dimensions)
 
-	// var height = dimensions[0];
-	// var width = dimensions[1];
-	// var depth = dimensions[2];
+	var height = dimensions[0];
+	var width = dimensions[1];
+	var depth = dimensions[2];
 
-	// var dim_0 = math.mean(mat,0);
-	// var dim_1 = math.mean(mat,1);
-	// var dim_2 = math.mean(mat,2);
-
-	var m = math.matrix(data.dim_0)
-	console.log(m)
-
-	m = math.reshape(m, [16,16,4])
-	// m = math.resize(m,[24,24])
-	console.log('hello')
-	console.log(math.mean(m,0))
-	console.log(math.mean(m,1))
-	console.log(math.mean(m,2))
-
-	// console.log(dim_1)
-	// console.log(dim_2)
-
+	// Obtain mean across dimension
+	var dim_0 = math.mean(mat,0);
+	var dim_1 = math.mean(mat,1);
+	var dim_2 = math.mean(mat,2);
 
 
 }
 
-function readMRI() {
-	var mri_data;
-	
+function resize() {
+	// Function for checking resizing of image.
 
 	var dim_0_64 = math.matrix(data_64.dim_0)
 	var dim_1_64 = math.matrix(data_64.dim_1)
@@ -118,10 +118,33 @@ function readMRI() {
 
 	math.reshape(dim_0_64, [64,64])
 
-	// pica.resizeBuffer(dim_0_64.toArray(), 32, 32).then((image)=>console.log('Success')).catch((err)=>console.log(err))
+	// pica's resize buffer apparently not a function. But its present in the documentation
+
+	pica.resizeBuffer(dim_0_64.toArray(), 32, 32).then((image)=>console.log('Success')).catch((err)=>console.log(err))
+
+
+	// jimp needs mime string in buffer
+
+	// var dim_0_64_resized = jimp.read(Buffer.from(dim_0_64.toArray())).then(function (image) {
+ //    // console.log('Sucess')
+ 	//	return image.resize(256, 256)
+	// }).catch(function (err) {
+	//     // handle an exception
+	// });
+
+
+	// Sharp not browserifying
+
+	// sharp(Buffer.from(dim_0_64.toArray())
+ //  	.(32, 32)
 
 
 	// console.log(dim_0_64)
+	var dim_0_32 = math.matrix(data.dim_0)
+
+	// console.log(dim_0_64_resized.toArray().reduce((a,b)=>a+b,0));
+	console.log(dim_0_32.toArray().reduce((a,b)=>a+b,0));
+
 	
 	
 }
@@ -129,9 +152,11 @@ function readMRI() {
 
 async function test(mri) {
 
-	var dim_0_32 = math.matrix(data.dim_0)
-	var dim_1_32 = math.matrix(data.dim_1)
-	var dim_2_32 = math.matrix(data.dim_2)
+	// Function to test the model with given mri model
+
+	var dim_0_32 = math.matrix(mri.dim_0)
+	var dim_1_32 = math.matrix(mri.dim_1)
+	var dim_2_32 = math.matrix(mri.dim_2)
 
 	dim_0_32 = math.multiply(dim_0_32, 1/255);
 	dim_1_32 = math.multiply(dim_1_32, 1/255);
@@ -144,7 +169,11 @@ async function test(mri) {
 	var dims = [dim_0, dim_1, dim_2];
 
 	const prediction = model.predict(dims);
-	statusElement.innerText = `Prediction : ${prediction}`;
+	const label = mri.label;
+	console.log(prediction)
+	statusElement.innerText = `Prediction : ${prediction} , Actual : ${label}`;
+
+
 
 }
 
@@ -157,7 +186,7 @@ async function main() {
 		messageElement.innerText = 'Model Has Been Loaded';
 		})
 
-	await readMRI();
+	// await resize();
 
 	test(data);
 
